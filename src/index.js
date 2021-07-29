20a10b07d0ad9bc103b52a31094b28a1a56c5754
Emanuel López <timbislopez@gmail.com>
2021-07-29 07:29:13 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+/**
+ * TODO:
+ *
+ * Ocultar secciones descuentos y billetera
+ * Mostrar secciones según qué ícono del navbar se apreta
+ * Agregar clase active en ícono navbar
+ *
+ * Evento click del buscador
+ * Evento enter del buscador
+ * Leer valor del input de búsqueda
+ * Mostrar mensaje de cero resultados
+ *
+ * Cambiar el número de la billetera
+ *
+ * Agregar elementos (líneas de texto) en los descuentos
+ */
